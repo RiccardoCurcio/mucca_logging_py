7bea8e5c4ef1a13a270968e2e40ffff0678e543a
fefender <fefender@gmail.com>
2018-06-20 15:50:38 +0200

    change into static class

--- mucca_logging.py
+++ mucca_logging.py
@@ -1,38 +1,28 @@
 import sys
 from datetime import datetime
 import os
-from dotenv import load_dotenv
-from dotenv import find_dotenv
-import inspect
 
 class logging:
-    def __init__(self):
-        load_dotenv(find_dotenv())
-
-    def log_info(self, msg, file, line):
+    @staticmethod
+    def log_info(msg, file, line):
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        level = "info"
+        level = "INFO"
         service_name = os.getenv("SERVICE_NAME")
         sys.stderr.write('[{}] {} {} {}:{} "{}"\n'.format(now,level, service_name, file, line, msg))
-        return True
+        return None
 
-    def log_warning(self, msg, file, line):
+    @staticmethod
+    def log_warning(msg, file, line):
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        level = "warning"
+        level = "WARNING"
         service_name = os.getenv("SERVICE_NAME")
         sys.stderr.write('[{}] {} {} {}:{} "{}"\n'.format(now,level, service_name, file, line, msg))
-        return True
+        return None
 
-    def log_error(self, msg, file, line):
+    @staticmethod
+    def log_error(msg, file, line):
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        level = "error"
+        level = "ERROR"
         service_name = os.getenv("SERVICE_NAME")
         sys.stderr.write('[{}] {} {} {}:{} "{}"\n'.format(now,level, service_name, file, line, msg))
-        return True
-
-# log = logging()
-# # get frame
-# frame = inspect.currentframe()
-# log.log_info("info test", frame.f_code.co_filename, frame.f_lineno)
-# log.log_warning("warning test", frame.f_code.co_filename, frame.f_lineno)
-# log.log_error("error test", frame.f_code.co_filename, frame.f_lineno)
+        return None
