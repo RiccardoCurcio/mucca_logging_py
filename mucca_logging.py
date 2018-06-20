2e654b4daf9f03f6d9dd6550742afb770063ea8b
fefender <fefender@gmail.com>
2018-06-20 16:03:36 +0200

    color level added

--- mucca_logging.py
+++ mucca_logging.py
@@ -6,7 +6,7 @@ class logging:
     @staticmethod
     def log_info(msg, file, line):
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        level = "INFO"
+        level = "\x1B[32mINFO\x1B[0m"
         service_name = os.getenv("SERVICE_NAME")
         sys.stderr.write('[{}] {} {} {}:{} "{}"\n'.format(now,level, service_name, file, line, msg))
         return None
@@ -14,7 +14,7 @@ class logging:
     @staticmethod
     def log_warning(msg, file, line):
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        level = "WARNING"
+        level = "\x1B[33mWARNING\x1B[0m"
         service_name = os.getenv("SERVICE_NAME")
         sys.stderr.write('[{}] {} {} {}:{} "{}"\n'.format(now,level, service_name, file, line, msg))
         return None
@@ -22,7 +22,7 @@ class logging:
     @staticmethod
     def log_error(msg, file, line):
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        level = "ERROR"
+        level = "\x1B[31mERROR\x1B[0m"
         service_name = os.getenv("SERVICE_NAME")
         sys.stderr.write('[{}] {} {} {}:{} "{}"\n'.format(now,level, service_name, file, line, msg))
         return None
